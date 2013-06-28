c2290e9f0d525765ace83f0173c1717c1927e9ab
denismikulich <818181md>
2013-06-28 18:35:04 +0300

    commit

--- stuff-mvc/src/test/java/com/stuff/stuffapp/domain/DomainTests.java
+++ stuff-mvc/src/test/java/com/stuff/stuffapp/domain/DomainTests.java
@@ -10,18 +10,25 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * tests
+ * 
+ * @author Home
+ * 
+ */
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:root-context.xml" })
 public class DomainTests {
 
 	@Autowired
 	private SessionFactory sessionFactory;
-	
+
 	@Test
 	public void setUp() {
 		Assert.assertNotNull("session factory must be initialized", sessionFactory);
 	}
-	
+
 	@Test
 	public void stuffTest() {
 		Role role1 = new Role();
@@ -29,18 +36,18 @@ public class DomainTests {
 		role1 = saveRole(role1);
 		Assert.assertNotNull(role1.getId());
 		System.out.println(role1.toString());
-		
+
 		Role role2 = new Role();
 		role2.setName("role2");
 		role2 = saveRole(role2);
 		Assert.assertNotNull(role2.getId());
-		
+
 		System.out.println(role2.toString());
-		
+
 		Role loadedRole1 = getRole(role1.getId());
 		Assert.assertEquals(loadedRole1.getId(), role1.getId());
 	}
-	
+
 	@Transactional
 	private Role saveRole(Role role) {
 		Session session = sessionFactory.openSession();
@@ -48,11 +55,11 @@ public class DomainTests {
 		session.close();
 		return role;
 	}
-	
+
 	@Transactional
 	private Role getRole(Long id) {
 		Session session = sessionFactory.openSession();
-		Role role = (Role)session.load(Role.class, id);
+		Role role = (Role) session.load(Role.class, id);
 		session.close();
 		return role;
 	}
